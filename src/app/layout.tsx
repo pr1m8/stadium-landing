5b84c377e99ef83aab3e86a6c7d9a21ccec60f43
0rac130fD31phi <william.astley@algebraicwealth.com>
2025-04-27 08:46:02 -0400

    Added global styles, updated metadata for the portfolio, and integrated Footer component into the RootLayout for improved layout structure.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,14 @@
 import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Analytics } from "@vercel/analytics/react"
+import "@/app/globals.css"
+import { Inter } from "next/font/google"
+import Footer from "@/components/nav/footer"
+const inter = Inter({ subsets: ["latin"] })
 
+export const metadata = {
+  title: "Will Astley | Portfolio",
+  description: "Portfolio, Blog, and Projects",
+}
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
@@ -10,6 +18,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         {/* Vercel Analytics and Speed Insights */}
         <Analytics />
         <SpeedInsights />
+        <Footer />
       </body>
     </html>
   )
