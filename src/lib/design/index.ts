5319a3a4ad9a86a3be30e13ca7c65b4bb7fa37db
0rac130fD31phi <william.astley@algebraicwealth.com>
2025-04-27 08:57:51 -0400

    Updated import statements in index.ts to use consistent double quotes for improved code style and readability.

--- src/lib/design/index.ts
+++ src/lib/design/index.ts
@@ -1,5 +1,5 @@
 // src/lib/design/index.ts
 // This file exports all design-related items for easier imports
 
-export * from './fonts';
-export * from './themes'; 
+export * from "./fonts";
+export * from "./themes";
